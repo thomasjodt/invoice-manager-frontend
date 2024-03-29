16d6c70adabca38641dc1df3857f99608510fd63
thomasjodt <jodt5200@gmail.com>
2024-03-28 20:41:43 -0500

    refactor(ui): Add anchor to the title in the sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,13 +1,18 @@
 import { Divider } from '@nextui-org/react'
 import { ShuffleIcon, DashboardIcon, UserIcon } from '@/components/icons'
 import { SideButton } from './SideButton'
+import { NavLink } from 'react-router-dom'
 
 export const Sidebar: React.FC = function () {
   return (
     <aside className='border-r p-4'>
-      <section className='p-3 flex gap-4 items-center text-purple-800'>
-        <ShuffleIcon size={24} />
-        <h3 className='font-semibold text-neutral-800 text-2xl'>Intifarma</h3>
+      <section>
+        <NavLink to='/' className='p-3 flex gap-4 items-center text-purple-800'>
+          <ShuffleIcon size={24} />
+          <h3 className='font-semibold text-neutral-800 text-2xl'>
+            Intifarma
+          </h3>
+        </NavLink>
       </section>
 
       <Divider className='my-8' />
