046a596ffa40219134f337fd392aa505f63c66c6
thomasjodt <jodt5200@gmail.com>
2024-03-28 19:26:32 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Invoices, Vendors } from '@/pages'
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { Layout } from './layout/Layout'
+import { NextUIProvider } from '@nextui-org/react'
+
+export const App: React.FC = () => {
+  const navigate = useNavigate()
+
+  return (
+    <NextUIProvider navigate={navigate}>
+      <Layout>
+        <Routes>
+          <Route path='invoices' element={<Invoices />} />
+          <Route path='vendors' element={<Vendors />} />
+          <Route path='*' element={<Navigate to='invoices' />} />
+        </Routes>
+      </Layout>
+    </NextUIProvider>
+  )
+}
