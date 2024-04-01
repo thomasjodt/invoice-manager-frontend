353e668c6495ae9907849b1a16009b577b15c2eb
thomasjodt <jodt5200@gmail.com>
2024-03-31 22:00:36 -0500

    feat(types): Create types file

---
+++ src/types/index.d.ts
@@ -0,0 +1,27 @@
+import { statuses } from '@/data'
+
+export interface Vendor {
+  id: number
+  name: string
+  fullName: string
+  balance: number
+}
+
+export interface Payment {
+  amount: number
+  id: number
+  invoiceId:number
+  paymentDate: string
+}
+
+export interface Invoice {
+  amount: number
+  dueDate: string
+  emissionDate: string
+  id: number
+  invoiceNumber: string
+  vendor: Omit<Vendor, 'balance'>
+  payments: Payment[]
+}
+
+export type Status = keyof typeof statuses
