d05832f7d62cac3d60dbaa33d86f903a1ef1084f
thomasjodt <jodt5200@gmail.com>
2024-04-01 21:13:50 -0500

    refactor(utils): Add constraint to dateFormat function to pass an argument in type string

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,9 +1,13 @@
-export const dateFormat = (date: Date): string => {
+export const dateFormat = (date: Date | string): string => {
   const formatter = Intl.DateTimeFormat('es-PE', {
     day: '2-digit',
     month: 'short',
     year: 'numeric'
   })
 
-  return formatter.format(date)
+  return formatter.format(
+    (typeof date === 'string')
+      ? new Date(date + 'T00:00')
+      : date
+    )
 }
