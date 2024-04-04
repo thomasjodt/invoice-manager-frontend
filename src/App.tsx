d1c790f9b6a957f12b1f6d1bca1590974c767d7a
thomasjodt <jodt5200@gmail.com>
2024-04-03 22:16:39 -0500

    feat(app): Add sub-routes to vendors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Invoices, Vendors } from '@/pages'
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
-import { Layout } from './layout/Layout'
 import { NextUIProvider } from '@nextui-org/react'
-import { Payments } from './pages/Payments'
+
+import { Layout } from './layout/Layout'
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import { Invoices, Vendors, VendorForm, NewVendor, Payments } from '@/pages'
 
 export const App: React.FC = () => {
   const navigate = useNavigate()
@@ -12,7 +12,12 @@ export const App: React.FC = () => {
       <Layout>
         <Routes>
           <Route path='invoices' element={<Invoices />} />
-          <Route path='vendors' element={<Vendors />} />
+          <Route path='vendors'>
+            <Route index element={<Vendors />}/>
+            <Route path='new' element={<NewVendor />} />
+            <Route path=':id' element={<VendorForm />} />
+            <Route path='0' element={<Navigate to='/vendors/new' />} />
+          </Route>
           <Route path='payments' element={<Payments />} />
           <Route path='*' element={<Navigate to='invoices' />} />
         </Routes>
