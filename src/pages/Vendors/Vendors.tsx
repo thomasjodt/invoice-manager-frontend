8aeefe5681ee96a2cf2ac1b47790f8cab5a90691
thomasjodt <jodt5200@gmail.com>
2024-04-07 23:13:02 -0500

    feat(vendors): Change new route to modal component to create vendor

--- src/pages/Vendors/Vendors.tsx
+++ src/pages/Vendors/Vendors.tsx
@@ -1,36 +1,43 @@
-import axios from 'axios'
-import { Button, Link } from '@nextui-org/react'
 import { useEffect, useState } from 'react'
+import { Button, useDisclosure } from '@nextui-org/react'
 
+import { VendorsApi } from '@/api'
 import type { Vendor } from '@/types'
-import { PlusIcon } from '@/components/icons'
 import { Header } from '@/components/ui'
-import { VendorCard } from './components'
-
-const url = 'http://localhost:8080/invoices/api/vendors/balance'
+import { PlusIcon } from '@/components/icons'
+import { NewVendorModal, VendorCard } from './components'
 
 export const Vendors: React.FC = function () {
   const [vendors, setVendors] = useState<Vendor[]>([])
+  const { isOpen, onOpenChange, onOpen } = useDisclosure()
+
+  const updateVendors = (vendor: Vendor): void => {
+    setVendors([...vendors, vendor])
+  }
+
+  const handleUpdate = (vendor: Vendor): void => {
+    const updatedVendors = vendors.map(v => {
+      return (v.id === vendor.id) ? { ...v, ...vendor } : v
+    })
+    setVendors(updatedVendors)
+  }
 
   useEffect(() => {
-    const getUsers = async (): Promise<void> => {
-      const { data, status } = await axios.get<Vendor[]>(url)
-
-      if (status !== 200) {
-        throw new Error('Hubo un error al conseguir los proveedores.')
-      }
-
-      setVendors(data)
-    }
-
-    getUsers()
-      .catch(console.error)
+    (async () => {
+      setVendors(await VendorsApi.getVendors())
+    })().catch(console.error)
   }, [])
 
   return (
     <>
+      <NewVendorModal
+        isOpen={isOpen}
+        update={updateVendors}
+        onOpenChange={onOpenChange}
+      />
+
       <Header title='Vendors'>
-        <Button href='/vendors/new' color='primary' as={Link} variant='solid'>
+        <Button color='primary' variant='solid' onClick={onOpen}>
           Add Vendor
           <PlusIcon size={18} />
         </Button>
@@ -43,7 +50,13 @@ export const Vendors: React.FC = function () {
       }>
         {
           (vendors.length > 0)
-            ? vendors.map(vendor => <VendorCard key={vendor.id} vendor={vendor} />)
+            ? vendors.map(vendor =>
+              <VendorCard
+                key={vendor.id}
+                vendor={vendor}
+                updateVendors={handleUpdate}
+              />
+            )
             : <p className='mt-32 font-semibold text-neutral-400'>No se encuentran Proveedores</p>
         }
       </section>
