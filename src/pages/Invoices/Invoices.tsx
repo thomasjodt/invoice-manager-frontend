7c9db500aa61699b8030f9ff5c4d214c56045b68
thomasjodt <jodt5200@gmail.com>
2024-04-07 23:09:34 -0500

    refactor(invoices): Change http to api method and Add a modal to create invoice

--- src/pages/Invoices/Invoices.tsx
+++ src/pages/Invoices/Invoices.tsx
@@ -1,32 +1,47 @@
 import { useEffect, useState } from 'react'
+import { Button, useDisclosure } from '@nextui-org/react'
 
+import { InvoicesApi } from '@/api'
 import type { Invoice } from '@/types'
-import { Header } from '@/components/ui/Header'
-import { InvoicesCard } from './components'
-import { http } from '@/data'
-import { Button } from '@nextui-org/react'
 import { PlusIcon } from '@/components/icons'
+import { Header } from '@/components/ui'
+import { InvoicesCard, NewInvoiceModal } from './components'
 
 export const Invoices: React.FC = function () {
   const [invoices, setInvoices] = useState<Invoice[]>([])
 
-  useEffect(() => {
-    const getInvoices = async (): Promise<void> => {
-      const { data } = await http.get<Invoice[]>('/invoices')
-      setInvoices(data)
-    }
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
-    getInvoices().catch(console.log)
+  const updateInvoices = (invoice: Invoice): void => {
+    setInvoices([...invoices, invoice])
+  }
+
+  useEffect(() => {
+    (async () => {
+      setInvoices(await InvoicesApi.getInvoices())
+    })().catch(console.log)
   }, [])
 
   return (
     <>
+    <NewInvoiceModal
+      isOpen={isOpen}
+      update={updateInvoices}
+      onOpenChange={onOpenChange}
+    />
+
       <Header title='Invoices'>
-        <Button color='primary' endContent={<PlusIcon />}>Create Invoice</Button>
+        <Button
+          color='primary'
+          onClick={onOpen}
+          endContent={<PlusIcon />}
+        >
+          Create Invoice
+        </Button>
       </Header>
 
       <section className='mt-20 px-5 grid gap-3 lg:grid-cols-2 xl:grid-cols-3 pb-10'>
-        {invoices.map(invoice => (
+        {invoices.map((invoice) => (
           <InvoicesCard
             key={invoice.id}
             invoice={invoice}
