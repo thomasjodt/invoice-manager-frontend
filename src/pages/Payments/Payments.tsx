9810b47c5a7e05a5e4a99c910d26597b3635d7f9
thomasjodt <jodt5200@gmail.com>
2024-04-09 20:26:28 -0500

    refactor(payments): Remove unnecessary imports and hooks

--- src/pages/Payments/Payments.tsx
+++ src/pages/Payments/Payments.tsx
@@ -1,29 +1,11 @@
-import { useEffect, useState } from 'react'
-
-import { http } from '@/data'
 import { Header } from '@/components/ui'
 import { PaymentCard } from './components/PaymentCard'
-import type { FullPayment, Invoice, Payment } from '@/types'
 import { Button } from '@nextui-org/react'
 import { PlusIcon } from '@/components/icons'
+import { usePaymentsContext } from './context'
 
 export const Payments: React.FC = function () {
-  const [payments, setPayments] = useState<FullPayment[]>([])
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await http.get<Payment[]>('/payments')
-      const fullPayments: FullPayment[] = []
-
-      for (const { amount, id, invoiceId, paymentDate} of data) {
-        const { data: invoice } = await http.get<Invoice>(`/invoices/${invoiceId}`)
-        const fullPayment: FullPayment = { id, amount, invoice, paymentDate }
-        fullPayments.push(fullPayment)
-      }
-
-      setPayments(fullPayments)
-    })()
-  }, [])
+  const { payments } = usePaymentsContext()
 
   return (
     <>
