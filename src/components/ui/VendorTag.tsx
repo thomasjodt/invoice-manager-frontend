d10073fbb3eda1516fb437d6c46ca74263a1b671
thomasjodt <jodt5200@gmail.com>
2024-04-09 19:24:01 -0500

    styles(vendors): Change text alignment to start

--- src/components/ui/VendorTag.tsx
+++ src/components/ui/VendorTag.tsx
@@ -8,7 +8,7 @@ export const VendorTag: React.FC<Props> = function ({ vendor, ...props }) {
   return (
     <div className='inline-flex flex-col items-start' {...props}>
       <p className='text-small text-inherit'>{vendor.name}</p>
-      <p className='text-tiny text-foreground-400'>{vendor.fullName}</p>
+      <p className='text-tiny text-foreground-400 text-start'>{vendor.fullName}</p>
     </div>
   )
 }
