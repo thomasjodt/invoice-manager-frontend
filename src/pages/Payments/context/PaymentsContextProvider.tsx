f74e6d5004fa09b9edb15daa529d8661253a7f72
thomasjodt <jodt5200@gmail.com>
2024-04-09 20:25:16 -0500

    feat(payments): Create the context for the payments and the custom hook to call this context

---
+++ src/pages/Payments/context/PaymentsContextProvider.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, createContext, useContext } from 'react'
+
+import type { PaymentsContextType } from '@/types'
+import { usePayments } from '../hooks'
+
+export const PaymentsContext = createContext((null as unknown )as PaymentsContextType)
+
+interface Props {
+  children: ReactNode
+}
+
+export const PaymentsContextProvider: React.FC<Props> = function ({ children }) {
+  const { payments, create, getAll, getOne, update, remove } = usePayments()
+  return (
+    <PaymentsContext.Provider value={{ create, getAll, getOne, payments, remove, update }}>
+      {children}
+    </PaymentsContext.Provider>
+  )
+}
+
+export const usePaymentsContext = (): PaymentsContextType => useContext(PaymentsContext)
---
+++ src/pages/Payments/context/index.tsx
@@ -0,0 +1,1 @@
+export { PaymentsContextProvider, usePaymentsContext } from './PaymentsContextProvider'
