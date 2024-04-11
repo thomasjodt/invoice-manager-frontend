f050825e4aaf97e00cc76377a37a7e93e6cd3cd3
thomasjodt <jodt5200@gmail.com>
2024-04-10 19:55:16 -0500

    feat(App): Surround app with contexts

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,29 @@ import { Layout } from './layout/Layout'
 import { Invoices, Vendors, Payments } from '@/pages'
 import { VendorContextProvider } from './pages/Vendors/context/VendorContext'
 import { PaymentsContextProvider } from './pages/Payments/context'
+import { InvoicesContextProvider } from './pages/Invoices/context'
 
 export const App: React.FC = () => {
   const navigate = useNavigate()
 
   return (
     <NextUIProvider navigate={navigate}>
-      <Layout>
-        <Routes>
-          <Route path='invoices' element={<Invoices />} />
-          <Route path='vendors' element={
-            <VendorContextProvider>
-              <Vendors />
-            </VendorContextProvider>
-          } />
-          <Route path='payments' element={
-            <PaymentsContextProvider>
-              <Payments />
-            </PaymentsContextProvider>
-          } />
-          <Route path='*' element={<Navigate to='invoices' />} />
-        </Routes>
-      </Layout>
+      <InvoicesContextProvider>
+        <VendorContextProvider>
+          <Layout>
+            <Routes>
+              <Route path='invoices' element={<Invoices />} />
+              <Route path='vendors' element={<Vendors />} />
+              <Route path='payments' element={
+                <PaymentsContextProvider>
+                  <Payments />
+                </PaymentsContextProvider>
+              } />
+              <Route path='*' element={<Navigate to='invoices' />} />
+            </Routes>
+          </Layout>
+        </VendorContextProvider>
+      </InvoicesContextProvider>
     </NextUIProvider>
   )
 }
