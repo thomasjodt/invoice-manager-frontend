68f5349622d0b84bf83a1a2addf36056c0c7b37b
thomasjodt <jodt5200@gmail.com>
2024-04-10 19:51:35 -0500

    feat(invoices): Create context for invoices

---
+++ src/pages/Invoices/context/InvoicesContext.tsx
@@ -0,0 +1,20 @@
+import { InvoicesContextType } from '@/types'
+import React, { ReactNode, createContext, useContext } from 'react'
+import { useInvoices } from '../hooks'
+
+export const InvoicesContext = createContext((null as unknown) as InvoicesContextType)
+
+interface Props {
+  children: ReactNode
+}
+export const InvoicesContextProvider: React.FC<Props> = ({ children }) => {
+  const { invoices, create, getAll, getOne, remove, update } = useInvoices()
+
+  return (
+    <InvoicesContext.Provider value={{ invoices, create, getAll, getOne, remove, update }}>
+      {children}
+    </InvoicesContext.Provider>
+  )
+}
+
+export const useInvoicesContext = (): InvoicesContextType => useContext(InvoicesContext)
---
+++ src/pages/Invoices/context/index.tsx
@@ -0,0 +1,1 @@
+export { InvoicesContextProvider, useInvoicesContext} from './InvoicesContext'
