8f2973e20f61e1f758137edc29c8ec3f187fa9b7
thomasjodt <jodt5200@gmail.com>
2024-04-15 22:25:48 -0500

    refactor(invoices): Add invoice prop for contextActions

--- src/pages/Invoices/components/StatusChip.tsx
+++ src/pages/Invoices/components/StatusChip.tsx
@@ -1,17 +1,24 @@
-import { statuses } from '@/data'
-import type { Status } from '@/types'
 import { Chip } from '@nextui-org/react'
+
+import { statuses } from '@/data'
+import { ContextActions } from './ContextActions'
+import type { Invoice, Status } from '@/types'
 
 interface Props {
   status: Status
+  invoice: Invoice
 }
 
-export const StatusChip: React.FC<Props> = function ({ status }) {
-  const color = statuses[status]
-
+export const StatusChip: React.FC<Props> = function ({ status, invoice }) {
   return (
-    <Chip radius='sm' variant='flat' color={color} className={`border min-w-[80px] text-center`}>
-      {status}
-    </Chip>
+    <div className='flex gap-1'>
+      <Chip radius='sm' variant='flat' color={statuses[status]} className={`min-w-[80px] text-center`}>
+        {status}
+      </Chip>
+
+      <ContextActions
+        invoice={invoice}
+      />
+    </div>
   )
 }
