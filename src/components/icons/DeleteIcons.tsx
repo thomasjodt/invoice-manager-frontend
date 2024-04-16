3180c8d1fd602966efceb023ad4742093d84f8b0
thomasjodt <jodt5200@gmail.com>
2024-04-15 22:23:52 -0500

    feat(icons): Add icon and add props to existent ones

--- src/components/icons/DeleteIcons.tsx
+++ src/components/icons/DeleteIcons.tsx
@@ -1,12 +1,16 @@
-export const DeleteIcon: React.FC = () => (
+interface Props {
+  size?: number
+}
+
+export const DeleteIcon: React.FC<Props> = ({ size = 24 }) => (
   <svg
     aria-hidden='true'
     fill='none'
     focusable='false'
-    height='1em'
+    height={size}
+    width={size}
     role='presentation'
     viewBox='0 0 20 20'
-    width='1em'
   >
     <path
       d='M17.5 4.98332C14.725 4.70832 11.9333 4.56665 9.15 4.56665C7.5 4.56665 5.85 4.64998 4.2 4.81665L2.5 4.98332'
--- src/components/icons/EditIcon.tsx
+++ src/components/icons/EditIcon.tsx
@@ -1,12 +1,17 @@
-export const EditIcon: React.FC = () => (
-  <svg
+interface Props {
+  size?: number
+}
+
+export const EditIcon: React.FC<Props> = function ({ size = 24 }) {
+  return (
+    <svg
     aria-hidden='true'
     fill='none'
     focusable='false'
-    height='1em'
+    height={size}
+    width={size}
     role='presentation'
     viewBox='0 0 20 20'
-    width='1em'
   >
     <path
       d='M11.05 3.00002L4.20835 10.2417C3.95002 10.5167 3.70002 11.0584 3.65002 11.4334L3.34169 14.1334C3.23335 15.1084 3.93335 15.775 4.90002 15.6084L7.58335 15.15C7.95835 15.0834 8.48335 14.8084 8.74168 14.525L15.5834 7.28335C16.7667 6.03335 17.3 4.60835 15.4583 2.86668C13.625 1.14168 12.2334 1.75002 11.05 3.00002Z'
@@ -33,4 +38,5 @@ export const EditIcon: React.FC = () => (
       strokeWidth={1.5}
     />
   </svg>
-)
+  )
+}
