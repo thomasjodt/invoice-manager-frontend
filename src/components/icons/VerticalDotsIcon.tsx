99fa35ee468f21f73c23e8d058c43e5f1f16afdc
thomasjodt <jodt5200@gmail.com>
2024-04-15 22:28:44 -0500

    styles(icons): Add className prop

--- src/components/icons/VerticalDotsIcon.tsx
+++ src/components/icons/VerticalDotsIcon.tsx
@@ -1,8 +1,9 @@
 interface Props extends React.ComponentProps<'svg'> {
   size?: number
+  className?: string
 }
 
-export const VerticalDotsIcon: React.FC<Props> = function ({ size = 24 }) {
+export const VerticalDotsIcon: React.FC<Props> = function ({ size = 24, className = '' }) {
   return (
     <svg
       aria-hidden='true'
@@ -12,6 +13,7 @@ export const VerticalDotsIcon: React.FC<Props> = function ({ size = 24 }) {
       role='presentation'
       viewBox='0 0 24 24'
       width={size}
+      className={className}
     >
       <path
         d='M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z'
