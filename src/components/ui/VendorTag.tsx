6e11be2355a08772f8dc92b0fb1bf32ed31e8b81
thomasjodt <jodt5200@gmail.com>
2024-04-16 22:49:12 -0500

    style(vendor): Align name to start

--- src/components/ui/VendorTag.tsx
+++ src/components/ui/VendorTag.tsx
@@ -12,7 +12,7 @@ interface Props {
 export const VendorTag: React.FC<Props> = function ({ vendor, classNames }) {
   return (
     <div className={`inline-flex flex-col items-start ${classNames?.container}`}>
-      <p className={`text-small text-inherit ${classNames?.name}`}>{vendor.name}</p>
+      <p className={`text-small text-inherit text-start ${classNames?.name}`}>{vendor.name}</p>
       <p className={`text-tiny text-foreground-400 text-start ${classNames?.fullName}`}>{vendor.fullName}</p>
     </div>
   )
