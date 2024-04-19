c08b1a38f26f5fcce40681acf64ebc22bb12f2f1
thomasjodt <jodt5200@gmail.com>
2024-04-18 21:57:46 -0500

    fix(invoices): Fix status message display

--- src/pages/Invoices/utils/getStatus.ts
+++ src/pages/Invoices/utils/getStatus.ts
@@ -2,12 +2,11 @@ import type { Status } from '@/types'
 
 export const getStatus = (dueDate: Date, balance: number): Status => {
   let status: Status
-  if (dueDate > new Date()) {
-    status = 'pending'
-  } else if (balance > 0) {
-    status = 'overdue'
-  } else {
+
+  if (balance === 0) {
     status = 'paid'
+    return status
   }
-  return status
+
+  return (dueDate > new Date()) ? 'pending' : 'overdue'
 }
