5791aa706c9b6c9d39e0b0b049159c017ec30acd
thomasjodt <jodt5200@gmail.com>
2024-04-18 22:52:22 -0500

    style(payments): Change the view from table to cards

--- src/pages/Payments/Payments.tsx
+++ src/pages/Payments/Payments.tsx
@@ -32,13 +32,7 @@ export const Payments: React.FC = function () {
 
       <NewPaymentModal isOpen={isOpen} onOpenChange={onOpenChange} />
 
-      <section className='grid gap-3 p-5'>
-        <div className='grid grid-cols-3 place-items-center bg-primary-100 rounded-md font-semibold p-1 text-neutral-600'>
-          <p className='place-self-start pl-8'>Vendor</p>
-          <p>Amount</p>
-          <p>Payment Date</p>
-        </div>
-
+      <section className='grid gap-3 p-5 lg:grid-cols-2 2xl:grid-cols-3'>
         {
           payments.map((payment) => (
             <PaymentCard key={payment.id} payment={payment} />
