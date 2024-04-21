6fcf8789f2c62a07e612ac1c825302af1bc4e993
thomasjodt <jodt5200@gmail.com>
2024-04-21 18:54:53 -0500

    feat(vendors): Implement all features

--- src/pages/Vendors/Vendors.tsx
+++ src/pages/Vendors/Vendors.tsx
@@ -1,35 +1,48 @@
-import { useEffect, useState } from 'react'
-import { Button, Pagination, useDisclosure } from '@nextui-org/react'
+import { useCallback, useEffect, useState } from 'react'
+import { Button, Card, Pagination, useDisclosure } from '@nextui-org/react'
 
 import { Header } from '@/components/ui'
 import { useVendorContext } from '@/context'
 import { PlusIcon } from '@/components/icons'
 import { FilterBar } from './components/filter'
-import type { ApiResponse, Vendor } from '@/types'
+import type { Vendor } from '@/types'
 import { NewVendorModal, VendorCard } from './components'
 
 export const Vendors: React.FC = function () {
-  const { getAll } = useVendorContext()
+  const { getAll, getByName } = useVendorContext()
   const [page, setPage] = useState(1)
   const [pages, setPages] = useState(1)
   const [itemsPerPage, setItemsPerPage] = useState(5)
   const { isOpen, onOpenChange, onOpen } = useDisclosure()
 
-  const [{ count, data: vendors }, setResponse] = useState<ApiResponse<Vendor[]>>({ count: 0, data: [], next: '', previous: '' })
+  const [vendors, setVendors] = useState<Vendor[]>([])
+  const [count, setCount] = useState(0)
+
+  const handleSearch = useCallback((name: string): void => {
+    if (name.trim() === '') return
+
+    getByName(name)
+      .then((res) => {
+        setVendors(res.data)
+        setCount(res.count)
+      })
+      .catch(console.error)
+  }, [getByName])
 
   useEffect(() => {
     const getAllVendors = async (): Promise<void> => {
-      const res = await getAll(page, itemsPerPage)
-      setResponse(res)
+      const { data, count } = await getAll(page, itemsPerPage)
 
       const div = count / itemsPerPage
       const extraPage = Number.isInteger(div) ? 0 : 1
 
       setPages(Math.floor(div) + extraPage)
+      setVendors(data)
+      setCount(count)
     }
 
     getAllVendors().catch(console.error)
-  }, [page, getAll, itemsPerPage, count])
+  }, [page, getAll, itemsPerPage])
 
   return (
     <>
@@ -46,49 +59,57 @@ export const Vendors: React.FC = function () {
       </Header>
 
       <div>
-        <FilterBar />
-        {(count > 0) && (
-          <div className='text-neutral-500 font-semibold text-sm mx-5 mt-4 flex justify-between'>
-            <p>Total {count} vendors</p>
+        <FilterBar onSearch={handleSearch} />
 
-            <div className='flex items-center gap-3'>
-              <p>Vendors per page</p>
-              <select onChange={(e) => { setItemsPerPage(Number(e.target.value)) }} className='border rounded-md p-1'>
-                <option value='5'>5</option>
-                <option value='10'>10</option>
-                <option value='15'>15</option>
-              </select>
+        <div className='max-w-xl lg:max-w-4xl mx-auto'>
+          {(count > 0) && (
+            <div className='text-neutral-500 font-semibold text-sm mx-5 my-4 flex justify-between'>
+              <p>Total {count} vendors</p>
+
+              <div className='flex items-center gap-3'>
+                <p>Vendors per page</p>
+                <select onChange={(e) => { setItemsPerPage(Number(e.target.value)) }} className='border rounded-md p-1'>
+                  <option value='5'>5</option>
+                  <option value='10'>10</option>
+                  <option value='15'>15</option>
+                </select>
+              </div>
             </div>
-          </div>
-        )}
-        <section className={
-          (vendors.length > 0)
-            ? 'grid gap-3 p-5 lg:p-8 2xl:p-15 lg:grid-cols-2 2xl:grid-cols-3'
-            : ' flex justify-center items-center'
-        }
-        >
-          {
-            (vendors.length > 0)
-              ? vendors.map(vendor =>
-                <VendorCard
-                  key={vendor.id}
-                  vendor={vendor}
+          )}
+          <Card
+            shadow='none' className={
+              (vendors.length > 0)
+                ? 'gap-3 p-5 lg:p-8 2xl:p-15 border min-h-[500px] justify-between mt-5'
+                : ' flex justify-center items-center min-h-[500px] mt-5'
+            }
+          >
+            <div className='flex flex-col gap-3'>
+              {
+                (vendors.length > 0)
+                  ? vendors.map(vendor =>
+                    <VendorCard
+                      key={vendor.id}
+                      vendor={vendor}
+                    />
+                  )
+                  : <p className='font-semibold text-neutral-400'>No se encuentran Proveedores</p>
+              }
+            </div>
+
+            {
+              (pages > 1 && page !== 0 && count > itemsPerPage) && (
+                <Pagination
+                  page={page}
+                  showControls
+                  total={pages}
+                  onChange={setPage}
+                  className='max-w-fit mx-auto'
                 />
               )
-              : <p className='mt-32 font-semibold text-neutral-400'>No se encuentran Proveedores</p>
-          }
-        </section>
-        {
-          (pages > 1 && page !== 0 && vendors.length > 0) && (
-            <Pagination
-              page={page}
-              showControls
-              total={pages}
-              onChange={setPage}
-              className='max-w-fit mx-auto'
-            />
-          )
-        }
+            }
+          </Card>
+        </div>
+
       </div>
     </>
   )
