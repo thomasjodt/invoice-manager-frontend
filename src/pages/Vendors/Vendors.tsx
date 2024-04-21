44860744852c3185bc0f1dea8e5a2f3fdee13de4
thomasjodt <jodt5200@gmail.com>
2024-04-21 07:24:08 -0500

    feat(vendors): Add a section that display the total count and set the items per page

--- src/pages/Vendors/Vendors.tsx
+++ src/pages/Vendors/Vendors.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Button, Pagination, useDisclosure } from '@nextui-org/react'
 
-import type { Vendor } from '@/types'
+import type { ApiResponse, Vendor } from '@/types'
 import { Header } from '@/components/ui'
 import { PlusIcon } from '@/components/icons'
 import { FilterBar } from './components/filter'
@@ -12,23 +12,24 @@ export const Vendors: React.FC = function () {
   const { getAll } = useVendorContext()
   const [page, setPage] = useState(1)
   const [pages, setPages] = useState(1)
-  const [itemsPerPage/* , setItemsPerPage */] = useState(5)
-  const [vendors, setVendors] = useState<Vendor[]>([])
+  const [itemsPerPage, setItemsPerPage] = useState(5)
   const { isOpen, onOpenChange, onOpen } = useDisclosure()
+
+  const [{ count, data: vendors }, setResponse] = useState<ApiResponse<Vendor[]>>({ count: 0, data: [], next: '', previous: '' })
 
   useEffect(() => {
     const getAllVendors = async (): Promise<void> => {
-      const { data, count } = await getAll(page, itemsPerPage)
+      const res = await getAll(page, itemsPerPage)
+      setResponse(res)
 
       const div = count / itemsPerPage
       const extraPage = Number.isInteger(div) ? 0 : 1
 
-      setVendors(data)
       setPages(Math.floor(div) + extraPage)
     }
 
     getAllVendors().catch(console.error)
-  }, [page, getAll, itemsPerPage])
+  }, [page, getAll, itemsPerPage, count])
 
   return (
     <>
@@ -44,8 +45,22 @@ export const Vendors: React.FC = function () {
         </Button>
       </Header>
 
-      <div className='grid h-fit'>
+      <div>
         <FilterBar />
+        {(count > 0) && (
+          <div className='text-neutral-500 font-semibold text-sm mx-5 mt-4 flex justify-between'>
+            <p>Total {count} vendors</p>
+
+            <div className='flex items-center gap-3'>
+              <p>Vendors per page</p>
+              <select onChange={(e) => { setItemsPerPage(Number(e.target.value)) }} className='border rounded-md p-1'>
+                <option value='5'>5</option>
+                <option value='10'>10</option>
+                <option value='15'>15</option>
+              </select>
+            </div>
+          </div>
+        )}
         <section className={
           (vendors.length > 0)
             ? 'grid gap-3 p-5 lg:p-8 2xl:p-15 lg:grid-cols-2 2xl:grid-cols-3'
@@ -64,7 +79,7 @@ export const Vendors: React.FC = function () {
           }
         </section>
         {
-          (pages > 1 && page !== 0) && (
+          (pages > 1 && page !== 0 && vendors.length > 0) && (
             <Pagination
               page={page}
               showControls
