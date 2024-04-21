11bb5ec0443d4e96fa4456e197163dd6daf9da86
thomasjodt <jodt5200@gmail.com>
2024-04-21 18:54:15 -0500

    feat(vendor): Add debouncing effect for searching a vendor by its name

--- src/pages/Vendors/components/filter/FilterBar.tsx
+++ src/pages/Vendors/components/filter/FilterBar.tsx
@@ -1,13 +1,21 @@
 import { SearchIcon } from '@/components/icons'
 import { Card, Input } from '@nextui-org/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface Props {
-  onChange?: () => void
+  onSearch: (name: string) => void
 }
 
-export const FilterBar: React.FC<Props> = function () {
+export const FilterBar: React.FC<Props> = function ({ onSearch }) {
   const [input, setInput] = useState('')
+
+  useEffect(() => {
+    const debounceId = setTimeout(() => {
+      onSearch(input)
+    }, 300)
+
+    return () => { clearTimeout(debounceId) }
+  }, [input, onSearch])
 
   return (
     <Card shadow='none' className='rounded-none border-b-1 p-3'>
