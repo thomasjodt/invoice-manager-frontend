cbf624fe43da5f308987a5bb0164425a8c01389f
thomasjodt <jodt5200@gmail.com>
2024-07-22 22:02:14 -0500

    feat: Add env variable types to import.meta.env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_BRAND_NAME: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
