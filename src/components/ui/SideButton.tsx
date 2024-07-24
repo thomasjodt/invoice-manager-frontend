da82b4b9ebb2b46c76cd3afc0b70e062e8acdcce
thomasjodt <jodt5200@gmail.com>
2024-07-23 23:48:30 -0500

    styles: Add Tooltip when sidebar is compacted.

--- src/components/ui/SideButton.tsx
+++ src/components/ui/SideButton.tsx
@@ -1,32 +1,46 @@
-import type { ReactNode } from 'react'
-import { Button } from '@nextui-org/react'
+import { useState, type ReactNode } from 'react'
+import { Button, Tooltip } from '@nextui-org/react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 interface Props {
   label: string
   link: string
   icon?: ReactNode
+  isOpen?: boolean
 }
 
-export const SideButton: React.FC<Props> = function ({ label, icon, link }) {
+export const SideButton: React.FC<Props> = function ({ label, icon, link, isOpen = true }) {
   const navigate = useNavigate()
   const location = useLocation()
+  const [isHovered, setIsHovered] = useState(false)
 
   const isActive = location.pathname.includes(link)
 
   if (icon !== undefined) {
     return (
-      <Button
-        size='lg'
-        fullWidth
-        variant={(isActive) ? 'flat' : 'light'}
+      <Tooltip
+        content={label}
+        placement='right'
+        className='py-3 px-5 font-semibold'
         color={isActive ? 'primary' : 'default'}
-        startContent={icon}
-        onClick={() => { navigate(link) }}
-        className='flex justify-start font-medium text-lg'
+        isOpen={!isOpen && isHovered}
+        onOpenChange={(state) => { setIsHovered(state) }}
+        closeDelay={0}
+        showArrow
       >
-        {label}
-      </Button>
+        <Button
+          size='lg'
+          fullWidth
+          variant={(isActive) ? 'flat' : 'light'}
+          color={isActive ? 'primary' : 'default'}
+          startContent={icon}
+          onClick={() => { navigate(link) }}
+          className={(isOpen) ? 'flex justify-start font-medium text-lg' : ''}
+          isIconOnly={!isOpen}
+        >
+          {(isOpen) && label}
+        </Button>
+      </Tooltip>
     )
   }
 }
