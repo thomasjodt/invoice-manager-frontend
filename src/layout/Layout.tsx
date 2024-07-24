363ebbbcc774b32b27d355267c14345f7c020461
thomasjodt <jodt5200@gmail.com>
2024-07-23 23:54:29 -0500

    styles: Add state to change the layout.

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,17 @@
 import { Sidebar } from '@/components/ui/Sidebar'
+import { useState } from 'react'
 
 interface Props {
   children: React.ReactNode
 }
 
 export const Layout: React.FC<Props> = function ({ children }) {
+  const [isOpen] = useState(true)
+
   return (
-    <div className='min-h-screen grid grid-cols-[300px_1fr] dark:bg-zinc-800'>
-      <Sidebar brand={import.meta.env.VITE_BRAND_NAME} />
-      <main>
+    <div className={`h-screen grid grid-cols-[${!isOpen ? 100 : 300}px_1fr] dark:bg-zinc-800`}>
+      <Sidebar isOpen={isOpen} brand={import.meta.env.VITE_BRAND_NAME} />
+      <main className='grid grid-rows-[100px_1fr] overflow-y-auto'>
         {children}
       </main>
     </div>
