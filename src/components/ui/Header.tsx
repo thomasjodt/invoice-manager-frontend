81f177c3d91588e8b3d5b9b75a90662bfa304684
thomasjodt <jodt5200@gmail.com>
2024-07-25 23:09:23 -0500

    feat: Add Burger Icon in the Header component to toggle the sidebar size

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
-// import { Button } from '@nextui-org/react'
-// import { BurgerIcon } from '../icons/BurgerIcon'
+import { Button } from '@nextui-org/react'
+import { BurgerIcon } from '../icons/BurgerIcon'
+import { useAppContext } from '@/context'
 
 interface Props {
   title: string
@@ -8,15 +9,18 @@ interface Props {
 }
 
 export const Header: React.FC<Props> = function ({ title, children }) {
+  const { toggleSidebar } = useAppContext()
+
   return (
     <>
       <header className='flex justify-between items-center p-6 border-b border-divider'>
         <div className='flex gap-2'>
-          {/* <Button
+          <Button
             isIconOnly
             variant='light'
             startContent={<BurgerIcon size={32} />}
-          /> */}
+            onPress={toggleSidebar}
+          />
           <h1 className='text-3xl text-neutral-700 dark:text-neutral-200 font-bold'>{title}</h1>
         </div>
         {children}
