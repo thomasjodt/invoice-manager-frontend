d26fa799a4120a9191514e3e421ed7cd5ce319b4
thomasjodt <jodt5200@gmail.com>
2024-07-25 23:10:15 -0500

    feat: Add the state to the Sidebar

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,18 @@
 import { Sidebar } from '@/components/ui/Sidebar'
-import { useState } from 'react'
+import { useAppContext } from '@/context'
 
 interface Props {
   children: React.ReactNode
 }
 
 export const Layout: React.FC<Props> = function ({ children }) {
-  const [isOpen] = useState(true)
+  const { isExpanded } = useAppContext()
 
-  const sidebarWidth = (isOpen) ? 'grid-cols-[300px_1fr]' : 'grid-cols-[100px_1fr]'
+  const sidebarWidth = (isExpanded) ? 'grid-cols-[300px_1fr]' : 'grid-cols-[80px_1fr]'
 
   return (
     <div className={`h-screen grid ${sidebarWidth} dark:bg-zinc-800`}>
-      <Sidebar isOpen={isOpen} brand={import.meta.env.VITE_BRAND_NAME} />
+      <Sidebar isOpen={isExpanded} brand={import.meta.env.VITE_BRAND_NAME} />
       <main className='grid grid-rows-[100px_1fr] overflow-y-auto'>
         {children}
       </main>
