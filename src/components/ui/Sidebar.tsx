d4b2fcb746c5e8cb29a8ac54833355fa7af43093
thomasjodt <jodt5200@gmail.com>
2024-07-25 23:11:16 -0500

    feat: Add new button to the bottom of the sidebar to toggle the theme and other improves

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -2,6 +2,8 @@ import { NavLink } from 'react-router-dom'
 
 import { SideButton } from './SideButton'
 import { UserIcon, ReceiptIcon, SunLowIcon, CashIcon } from '@/components/icons'
+import { useAppContext } from '@/context'
+import { MoonIcon } from '../icons/MoonIcon'
 
 interface Props {
   brand?: string
@@ -9,38 +11,53 @@ interface Props {
 }
 
 export const Sidebar: React.FC<Props> = function ({ brand, isOpen }) {
+  const { isDark, toggleTheme } = useAppContext()
+
   return (
     <aside className='border-r border-divider grid grid-rows-[100px_1fr]'>
       <section className='flex items-center justify-center border-b border-divider'>
         <NavLink to='/' className='p-3 flex gap-4 items-center text-yellow-500'>
           <SunLowIcon size={32} />
-          <h3 className='font-semibold text-neutral-800 text-2xl dark:text-neutral-200'>
-            {(isOpen === true) && brand}
-          </h3>
+          {(isOpen === true) && (
+            <h3 className='font-semibold text-neutral-800 text-2xl dark:text-neutral-200'>
+              {brand}
+            </h3>
+          )}
         </NavLink>
       </section>
 
-      <section className={`flex flex-col gap-1 pt-6 px-2 ${(isOpen === true) ? 'items-start' : 'items-center'}`}>
-        <SideButton
-          link='invoices'
-          label='Invoices'
-          isOpen={isOpen}
-          icon={<ReceiptIcon size={24} />}
-        />
+      <section className={`flex flex-col py-6 px-2 justify-between ${(isOpen === true) ? 'items-start' : 'items-center'}`}>
+        <div className='flex flex-col gap-1 w-full items-center'>
+          <SideButton
+            link='invoices'
+            label='Invoices'
+            isOpen={isOpen}
+            icon={<ReceiptIcon size={24} />}
+          />
 
-        <SideButton
-          link='vendors'
-          label='Vendors'
-          isOpen={isOpen}
-          icon={<UserIcon size={24} />}
-        />
+          <SideButton
+            link='vendors'
+            label='Vendors'
+            isOpen={isOpen}
+            icon={<UserIcon size={24} />}
+          />
 
-        <SideButton
-          link='payments'
-          label='Payments'
-          isOpen={isOpen}
-          icon={<CashIcon size={24} />}
-        />
+          <SideButton
+            link='payments'
+            label='Payments'
+            isOpen={isOpen}
+            icon={<CashIcon size={24} />}
+          />
+        </div>
+
+        <div className='flex flex-col gap-1 w-full items-center'>
+          <SideButton
+            label={`${(isDark) ? 'Light' : 'Dark'} theme`}
+            isOpen={isOpen}
+            icon={(isDark) ? <SunLowIcon /> : <MoonIcon />}
+            onClick={toggleTheme}
+          />
+        </div>
       </section>
     </aside>
   )
