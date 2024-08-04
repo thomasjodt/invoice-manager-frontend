24396d637bc24efb09443627e07458f6c280b67e
thomasjodt <jodt5200@gmail.com>
2024-08-03 23:42:25 -0500

    refactor: Change DeleteModal to customize content and actions

--- src/app/invoices/components/modals/DeleteModal.tsx
+++ src/app/invoices/components/modals/DeleteModal.tsx
@@ -1,12 +1,16 @@
+import { type ReactNode } from 'react'
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@nextui-org/react'
 
 interface Props {
+  title?: ReactNode
+  description?: ReactNode
   isModalOpen?: boolean
+  classNames?: Partial<Record<'title' | 'description', string>>
   onDelete?: () => void
   onCloseModal?: () => void
 }
 
-export const DeleteModal: React.FC<Props> = function ({ isModalOpen, onDelete, onCloseModal }) {
+export const DeleteModal: React.FC<Props> = function ({ title, description, isModalOpen, classNames, onDelete, onCloseModal }) {
   return (
     <Modal
       backdrop='opaque'
@@ -14,24 +18,22 @@ export const DeleteModal: React.FC<Props> = function ({ isModalOpen, onDelete, o
       onClose={onCloseModal}
     >
       <ModalContent>
-        {
-          () => (
-            <>
-              <ModalHeader>
-                <h3 className='font-semibold'>Are you sure to delete this invoice?</h3>
-              </ModalHeader>
+        {() => (
+          <>
+            <ModalHeader className={`text-default-900 ${(classNames?.title !== undefined) ? classNames.title : ''}`.trim()}>
+              {(title !== undefined) ? title : 'Are you absolutely sure?'}
+            </ModalHeader>
 
-              <ModalBody>
-                <p className='text-neutral-600'>This action will delete this invoice and all its corresponding payments.</p>
-              </ModalBody>
+            <ModalBody className={`text-default-600 ${(classNames?.description !== undefined) ? classNames.description : ''}`.trim()}>
+              {(description !== undefined) ? description : 'This action cannot be undone and will be permanent.'}
+            </ModalBody>
 
-              <ModalFooter className='flex justify-between'>
-                <Button variant='light' onPress={onCloseModal}>No, cancel</Button>
-                <Button color='danger' onPress={onDelete}>Yes, delete</Button>
-              </ModalFooter>
-            </>
-          )
-        }
+            <ModalFooter className='flex justify-between'>
+              <Button variant='light' onPress={onCloseModal}>No, cancel</Button>
+              <Button color='danger' onPress={onDelete}>Yes, delete</Button>
+            </ModalFooter>
+          </>
+        )}
       </ModalContent>
     </Modal>
   )
