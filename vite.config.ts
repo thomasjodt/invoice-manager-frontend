86b91cc338bc2447b46e3e096c7cdb828e85b129
thomasjodt <jodt5200@gmail.com>
2024-08-04 10:18:12 -0500

    feat: Add the necessary dependencies to make tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsConfigPaths()]
+  plugins: [react(), viteTsConfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'vitest.setup.ts'
+  }
 })
